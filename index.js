fae0b5ee9ebe8dfe3893dc4cfd38f25b5d5287df
Heather Arbuckle <bucklehead@gmail.com>
2019-05-16 21:27:52 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function takeANumber(line, name) {
+  return `Welcome, ${name}. You are number ${line-1} in line.`;
+}
+
+// function nowServing(katzDeliLine) {
+//   for (katzDeliLine[0]) {
+//     return `${name}`;
+//     katzDeliLine.shift();
+//   }
+// }
